e9ac31c351eef741b3ca1f58325ba35168ce30b3
HPDell <huyg0180110559@outlook.com>
2022-11-26 14:56:16 +0800

    add: test without openmp

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh -l
 
+### Normal Test
 mkdir build
 cmake -B build -S . && \
+    cmake --build build --config Release && \
+    ctest --test-dir build --output-on-failure
+
+### Test without OpenMP
+rm -rf build/*
+cmake -B build -S . -DENABLE_OpenMP=FALSE && \
     cmake --build build --config Release && \
     ctest --test-dir build --output-on-failure
