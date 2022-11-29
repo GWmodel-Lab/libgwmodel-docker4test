ee49ce238e7fc0aed5cf21392661c0a7a014318b
HPDell <huyg0180110559@outlook.com>
2022-11-29 18:38:07 +0800

    edit: change test script to debug mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 ### Normal Test
 mkdir build
-cmake -B build -S . && \
-    cmake --build build --config Release && \
+cmake -B build -S . -DCMAKE_BUILD_TYPE:STRING=Debug && \
+    cmake --build build --config Debug && \
     ctest --test-dir build --output-on-failure
 
 ### Test without OpenMP
 rm -rf build/*
-cmake -B build -S . -DENABLE_OpenMP=FALSE && \
-    cmake --build build --config Release && \
+cmake -B build -S . -DCMAKE_BUILD_TYPE:STRING=Debug -DENABLE_OpenMP=FALSE && \
+    cmake --build build --config Debug && \
     ctest --test-dir build --output-on-failure
